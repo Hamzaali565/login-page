8ac9a90be1496d1cb614c90914c5bb3b210115ec
Bilal Sami <bilalsamisaylani00@gmail.com>
2022-10-10 22:53:49 +0500

    login page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login';
+function App() {
+  return (
+    <div className="App">
+<Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./index.css"
+let Login = () =>{
+return(
+    <div className="main">
+{/* image */}
+<div className="image">
+
+</div>
+{/* form */}
+<div className="form">
+    <div className="LTC">
+    Login to continue
+    </div>
+    <div className="inputs">
+        <div><input type="text" placeholder="Email" name="" id="" /></div>
+        <div><input type="text" placeholder="Password" name="" id="" /></div>
+    </div>
+    <div className="remgot">
+        <div>Remember me</div>
+        <div className="mainFor">Forgot Password?</div>
+    </div>
+    <div className="logButton">
+        <button>LOGIN</button>
+    </div>
+    <div className="SL">
+    or sign up using
+    </div>
+    <div className="icons">
+<div><img src="https://cdn-icons-png.flaticon.com/512/37/37409.png" alt="" /></div>
+<div><img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png" alt="" /> </div>
+    </div>
+</div>
+
+    </div>
+)
+
+
+};
+export default Login
